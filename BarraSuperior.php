73d810738fb92d349e98d8c8bb0c231e0b2b43da
SitioWEB-ENPER <138508562+AngelPerezSMig@users.noreply.github.com>
2023-08-21 12:26:27 -0600

    Update BarraSuperior.php

--- BarraSuperior.php
+++ BarraSuperior.php
@@ -84,3 +84,108 @@
             </div>
         </div>
     </div>
+    <!-- Topbar End -->
+
+
+    <!-- Barra de navegación Start -->
+    <div class="container-fluid bg-dark mb-30">
+        <div class="row px-xl-5">
+            <div class="col-lg-3 d-none d-lg-block">
+                <a class="btn d-flex align-items-center justify-content-between bg-primary w-100" data-toggle="collapse" href="#navbar-vertical" style="height: 65px; padding: 0 30px;">
+                    <h6 class="text-dark m-0"><i class="fa fa-bars mr-2"></i>Categorías</h6>
+                    <i class="fa fa-angle-down text-dark"></i>
+                </a>
+                
+                <nav class="collapse position-absolute navbar navbar-vertical navbar-light align-items-start p-0 bg-light" id="navbar-vertical" style="width: calc(100% - 30px); z-index: 999;">
+                    <div class="navbar-nav w-100">
+                        <div class="nav-item dropdown dropright">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Nutrición<i class="fa fa-angle-right float-right mt-1"></i></a>
+                            <div class="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <a href="" class="dropdown-item">Vitaminas</a>
+                                <a href="" class="dropdown-item">Suplementos</a>
+                                <a href="" class="dropdown-item">Bebidas</a>
+                            </div>
+                        </div>
+                        <div class="nav-item dropdown dropright">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Belleza<i class="fa fa-angle-right float-right mt-1"></i></a>
+                            <div class="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <a href="" class="dropdown-item">Labiales</a>
+                                <a href="" class="dropdown-item">Mascarilla Pulidora, hidratante y de más.</a>
+                                <a href="" class="dropdown-item">Exfloliantes</a>
+                                <a href="" class="dropdown-item">Agua micelar</a>
+                                <a href="" class="dropdown-item">Delineadores</a>
+                               
+                            </div>
+                        </div>
+                        <div class="nav-item dropdown dropright">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Cuidado personal<i class="fa fa-angle-right float-right mt-1"></i></a>
+                            <div class="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <a href="" class="dropdown-item">Jabones en barra</a>
+                                <a href="" class="dropdown-item">Jabones coorporales</a>
+                                <a href="" class="dropdown-item">Shampoohs</a>
+                                <a href="" class="dropdown-item">Cepillos dentales</a>
+                                 <a href="" class="dropdown-item">Desodorantes</a>
+                                <a href="" class="dropdown-item">Cremas</a>
+                            </div>
+                        </div>
+                        <div class="nav-item dropdown dropright">
+                            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Cuidado del hogar<i class="fa fa-angle-right float-right mt-1"></i></a>
+                            <div class="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                                <a href="" class="dropdown-item"> Detergente en polvo</a>
+                                <a href="" class="dropdown-item">Detergente en liquido para ropa</a>
+                                <a href="" class="dropdown-item">Limpiador multiusos</a>
+                                 <a href="" class="dropdown-item">Liquido lavaplatos</a>
+                                <a href="" class="dropdown-item">Fibras</a>
+                            </div>
+                        </div>
+                        <!-- <a href="" class="nav-item nav-link">Shirts</a>
+                        <a href="" class="nav-item nav-link">Jeans</a>
+                        <a href="" class="nav-item nav-link">Swimwear</a>
+                        <a href="" class="nav-item nav-link">Sleepwear</a>
+                        <a href="" class="nav-item nav-link">Sportswear</a>
+                        <a href="" class="nav-item nav-link">Jumpsuits</a>
+                        <a href="" class="nav-item nav-link">Blazers</a>
+                        <a href="" class="nav-item nav-link">Jackets</a>
+                        <a href="" class="nav-item nav-link">Shoes</a> -->
+                    </div>
+                </nav>
+            </div>
+            <div class="col-lg-9">
+                <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">
+                    <a href="" class="text-decoration-none d-block d-lg-none">
+                        <span class="h1 text-uppercase text-dark bg-light px-2">EN</span>
+                        <span class="h1 text-uppercase text-light bg-primary px-2 ml-n1">PER</span>
+                    </a>
+                    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div class="navbar-nav mr-auto py-0">
+                            <a href="index.html" class="nav-item nav-link active">Inicio</a>
+                            <a href="shop.html" class="nav-item nav-link">Tienda</a>
+                            <a href="detail.html" class="nav-item nav-link">Detalles de productos</a>
+                            <div class="nav-item dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Compras<i class="fa fa-angle-down mt-1"></i></a>
+                                <div class="dropdown-menu bg-primary rounded-0 border-0 m-0">
+                                    <a href="cart.html" class="dropdown-item">Carrito de compras</a>
+                                    <a href="checkout.html" class="dropdown-item">Pedidos</a>
+                                </div>
+                            </div>
+                            <a href="contact.html" class="nav-item nav-link">Auspicio</a>
+                        </div>
+                        <!--Favoritos y carrito de compras-->
+                        <div class="navbar-nav ml-auto py-0 d-none d-lg-block">
+                            <a href="" class="btn px-0">
+                                <i class="fas fa-heart text-primary"></i>
+                                <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;">0</span>
+                            </a>
+                            <a href="" class="btn px-0 ml-3">
+                                <i class="fas fa-shopping-cart text-primary"></i>
+                                <span class="badge text-secondary border border-secondary rounded-circle" style="padding-bottom: 2px;">0</span>
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
