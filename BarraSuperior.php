481294b99e1bbbc8621545b614074b049cd48afe
SitioWEB-ENPER <138508562+AngelPerezSMig@users.noreply.github.com>
2023-08-21 12:05:14 -0600

    Crear archivo BarraSuperior.php

---
+++ BarraSuperior.php
@@ -0,0 +1,86 @@
+<div class="container-fluid">
+        <div class="row bg-secondary py-1 px-xl-5">
+            <div class="col-lg-6 d-none d-lg-block">
+                <div class="d-inline-flex align-items-center h-100">
+                    <a style="color: black;" class="nav-link dropdown-toggle" href="">Acerca de nosotros</a>
+                    <div class="nav-item dropdown dropright">
+                        <a style="color: black;" href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Contactanos</a>
+                        <div class="dropdown-menu position-absolute rounded-0 border-0 m-0">
+                            <p class="dropdown-item"><i class="fa fa-map-marker-alt text-primary mr-3"></i> C. 10 Nte., El Dulce Nombre, 41105 Chilapa de Álvarez, Gro.</p>
+                            <p class="dropdown-item"><i class="fa fa-envelope text-primary mr-3"></i></p>
+                            <p class="dropdown-item"><i class="fa fa-phone-alt text-primary mr-3"></i>+52 221 269 0224</p>
+                        </div>
+                    </div>
+                    
+                <!--<a class="text-body mr-3" href="">Help</a>-->
+                <!--<a class="text-body mr-3" href="">FAQs</a>-->
+                </div>
+            </div>
+            <div class="col-lg-6 text-center text-lg-right">
+                <div class="d-inline-flex align-items-center">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">Mi cuenta</button>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <button class="dropdown-item" type="button">Iniciar sesión</button>
+                            <button class="dropdown-item" type="button">Registrarse</button>
+                        </div>
+                    </div>
+
+                    <!-- para seleccionar la moneda e idioma-->
+                    <!--<div class="btn-group mx-2">
+                        <button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">USD</button>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <button class="dropdown-item" type="button">EUR</button>
+                            <button class="dropdown-item" type="button">GBP</button>
+                            <button class="dropdown-item" type="button">CAD</button>
+                        </div>
+                    </div>
+                    <div class="btn-group">
+                        button type="button" class="btn btn-sm btn-light dropdown-toggle" data-toggle="dropdown">EN</button>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <button class="dropdown-item" type="button">FR</button>
+                            <button class="dropdown-item" type="button">AR</button>
+                            <button class="dropdown-item" type="button">RU</button>
+                        </div>
+                    </div>-->
+                </div>
+
+                <!--carrito de compras-->
+                <div class="d-inline-flex align-items-center d-block d-lg-none">
+                    <a href="" class="btn px-0 ml-2">
+                        <i class="fas fa-heart text-dark"></i>
+                        <span class="badge text-dark border border-dark rounded-circle" style="padding-bottom: 2px;">0</span>
+                    </a>
+                    <a href="" class="btn px-0 ml-2">
+                        <i class="fas fa-shopping-cart text-dark"></i>
+                        <span class="badge text-dark border border-dark rounded-circle" style="padding-bottom: 2px;">0</span>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+        <div class="row align-items-center bg-light py-3 px-xl-5 d-none d-lg-flex">
+            <div class="col-lg-4">
+                <a href="" class="text-decoration-none">
+                    <span class="h1 text-uppercase text-primary bg-dark px-2">EN</span>
+                    <span class="h1 text-uppercase text-dark bg-primary px-2 ml-n1">PER</span>
+                </a>
+            </div>
+            <div class="col-lg-4 col-6 text-left">
+                <form action="">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Buscar Productos">
+                        <div class="input-group-append">
+                            <span class="input-group-text bg-transparent text-primary">
+                                <i class="fa fa-search"></i>
+                            </span>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-4 col-6 text-right">
+                <p class="m-0">Atención al cliente</p>
+                <h5 class="m-0">+52 2212690224</h5>
+            </div>
+        </div>
+    </div>
